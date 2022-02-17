0b5d6c8f4c208add04604f7a8123545fa6ae5b4c
wannabetrader <guillaumevial@vial.local>
2022-02-17 10:41:22 -0500

    ft

--- ft_atoi.c
+++ ft_atoi.c
@@ -17,9 +17,9 @@ int ft_atoi(char *str)
 	while (str[i] == '+' || str[i] == '-')
 	{
 		if (sign_met == 1)
-		break ;
+			break ;
 		if (str[i] == '-')
- 		sign *= -1;
+ 			sign *= -1;
 		sign_met = 1;
  		i++;
 	}
