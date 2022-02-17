cc4b0f46f5d40db0894417ce24e715073c61c78e
wannabetrader <guillaumevial@vial.local>
2022-02-17 11:35:10 -0500

    libft_update

--- libft.h
+++ libft.h
@@ -2,7 +2,15 @@
 #define LIBFT_H
 
 #include <unistd.h>
-void    ft_putchar(char c);
 void    ft_putstr(char *str);
-
+void    ft_putchar(char c);
+int     ft_strlen(char *str);
+int     ft_atoi(char *str);
+int     ft_isascii(char c);
+int     ft_isprint(char c);
+int     ft_isdigit(int c);
+int     ft_isalpha(int c);
+int     ft_toupper(int c);
+int	    ft_isalnum(int c);
+int     ft_tolower(int c);
 #endif
