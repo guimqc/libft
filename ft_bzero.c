4f2b926d92f10ad73bad6b28f963486d7e316ee6
wannabetrader <guillaumevial@vial.local>
2022-02-20 13:16:32 -0500

    ft_bzero_fix

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,6 +1,14 @@
 #include "libft.h"
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-    ft_memset(s, '0', n);
+	size_t  i;
+	char *converted_str;
+
+	converted_str = (char *)s;
+	if (n == 0)
+		return ;
+	i = -1;
+	while (++i < n)
+		converted_str[i] = 0;
 }
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -8,11 +8,8 @@ void    *ft_memcpy(void *dst, const void *src, size_t n)
 
 	converted_dst = (char *)dst;
 	converted_src = (char *)src;
-	i = 0;
-	while (i < n)
-	{
+	i = -1;
+	while (++i < n)
 		converted_dst[i] = converted_src[i];
-		i++;
-	}
 	return (dst);
 }
