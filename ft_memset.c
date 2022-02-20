4418443fab9dd63405d8ffbfeec1f1ac419af7eb
wannabetrader <guillaumevial@vial.local>
2022-02-19 20:17:10 -0500

    memset_upgrade

---
+++ ft_memset.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_memset(void *str, int c, size_t len)
+{
+	size_t i;
+	unsigned char *edited_str;
+	edited_str = (unsigned char *)str;
+	i = -1;
+	while (++i < len)
+		edited_str[i] = (unsigned char)c;
+	return	(edited_str);
+}
--- libft.h
+++ libft.h
@@ -4,15 +4,16 @@
 #include <unistd.h>
 
 size_t  ft_strlen(char *str);
-void    ft_putstr(char *str);
 void    ft_putchar(char c);
+void    ft_putstr(char *str);
+void   *ft_memset(void *str, int c, size_t len);
 int     ft_atoi(char *str);
 int     ft_isascii(char c);
 int     ft_isprint(char c);
 int     ft_isdigit(int c);
 int     ft_isalpha(int c);
-int     ft_toupper(int c);
 int	    ft_isalnum(int c);
+int     ft_toupper(int c);
 int     ft_tolower(int c);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 
