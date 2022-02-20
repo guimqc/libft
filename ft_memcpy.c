2d8cc0f9347a757ff910a3a54e0da2697cf49534
wannabetrader <guillaumevial@vial.local>
2022-02-20 02:55:58 -0500

    ft_memcpy_norm

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,18 +1,18 @@
 #include "libft.h"
 
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-    char *converted_src;
-    char *converted_dst;
-    size_t i;
+	char *converted_src;
+	char *converted_dst;
+	size_t i;
 
-    converted_dst = (char *)dst;
-    converted_src = (char *)src;
-    i = 0;
-    while (i < n)
-    {
-        converted_dst[i] = converted_src[i];
-        i++;
-    }
-    return (dst);
+	converted_dst = (char *)dst;
+	converted_src = (char *)src;
+	i = 0;
+	while (i < n)
+	{
+		converted_dst[i] = converted_src[i];
+		i++;
+	}
+	return (dst);
 }
