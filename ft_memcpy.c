4d999416d8027380e8a61259f947cc7ec738cf10
wannabetrader <guillaumevial@vial.local>
2022-02-20 14:56:34 -0500

    ft_mem_optimized

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,14 +2,10 @@
 
 void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char *converted_src;
-	char *converted_dst;
 	size_t i;
 
-	converted_dst = (char *)dst;
-	converted_src = (char *)src;
 	i = -1;
 	while (++i < n)
-		converted_dst[i] = converted_src[i];
+		((char *)dst)[i] = ((char *)src)[i];
 	return (dst);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,12 +3,10 @@
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t i;
-	char *converted_src = (char *) src;
-	char *converted_dst = (char *) dst;
 
 	i = -1;
 	while (++i < len)
-		if (converted_dst[i] && converted_src[i])
-			converted_dst[i] = converted_src[i];
+		if (((char *)dst)[i] && ((char *)src)[i])
+			((char *)dst)[i] = ((char *)src)[i];
 	return (dst);
 }
