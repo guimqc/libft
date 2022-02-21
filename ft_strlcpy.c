67020085a6b7c914883dbdaa71d5a6fe90bb6ae8
wannabetrader <guillaumevial@vial.local>
2022-02-20 20:52:28 -0500

    ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -7,7 +7,7 @@ size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (dstsize <= 0)
 		return (ft_strlen((char *)src));
 	i = -1;
-	while (++i < dstsize - 1)
+	while (++i < dstsize - 1 && src[i])
 		dst[i] = src[i];
 	dst[i] = '\0';
 	return ft_strlen((char *)src);
