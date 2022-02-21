4efc0fd9076698e953007916d47d2ebdcd441ecf
wannabetrader <guillaumevial@vial.local>
2022-02-20 20:37:52 -0500

    ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,8 +6,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 
     i = 0;
     while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
-    {
         i++;
-    }
     return (s1[i] - s2[i]);
 }
