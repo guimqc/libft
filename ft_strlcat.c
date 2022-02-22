4d0a269d538a32ecad25c23b84c464b259ed7389
vial <guillaumevial15@gmail.com>
2022-02-22 14:13:08 -0500

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,20 +1,33 @@
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t i;
-	size_t ii;
+	size_t dlen;
 
-	ii = ft_strlen(dst);
-	if (ii > dstsize)
+	dlen = ft_strlen(dst);
+	if (dlen > dstsize)
 		return (dstsize + ft_strlen((char *)src));
 	if (dstsize <= 0)
 		return (ft_strlen((char*)src));
 	i = -1;
-	while (++i < ii && src[i])
-		dst[ii++] = src[i];
-	dst[ii] = '\0';
+	while (++i < dlen - 1&& src[i])
+		dst[dlen++] = src[i];
+	dst[dlen] = '\0';
 	while (src[i])
 		i++;
-	return (ii + i);
+	return (dlen + i);
+}
+
+int main()
+{
+	char dst[12] = "hello";
+	char src[] = " world";
+	printf("initial dst = %s\n", dst);
+	printf("initial dst len = %zu\n", ft_strlen(dst));
+	strlcat(dst, src, 12);
+	printf("after strlcat dst = %s\n", dst);
+	printf("after strlcat dst len = %zu\n", ft_strlen(dst));
 }
