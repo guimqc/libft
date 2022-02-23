4133c1c9400cfb14b2bed7bd813b3516d4a2b717
vial <guillaumevial15@gmail.com>
2022-02-23 04:36:14 -0500

    strrchr_fix

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,7 +5,7 @@ char    *ft_strrchr(const char *s, int c)
 	int dstsize;
 
 	dstsize = ft_strlen((char *)s) + 1;
-	while (dstsize > 0)
+	while (dstsize > -1)
 		if (s[dstsize--] == (char)c)
 			return ((char *)s + ++dstsize);
 	return (NULL);
