1d0f945aaa8d6afebf9264a30082c78194ca6328
vial <guillaumevial15@gmail.com>
2022-02-23 16:03:44 -0500

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-int memcmp(const void *s1, const void *s2, size_t n)
+int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
     size_t i;
     unsigned char *str1;
@@ -8,10 +8,9 @@ int memcmp(const void *s1, const void *s2, size_t n)
 
     str1 = (unsigned char *)s1;
     str2 = (unsigned char *)s2;
-
-    i = 0;
-    while (i < n)
-        if (str1[i] == str2[i])
+    i = -1;
+    while (++i < n)
+        if (str1[i] != str2[i])
             return (str1[i] - str2[i]);
     return (0);
 }
--- libft.h
+++ libft.h
@@ -23,6 +23,7 @@ int     ft_isalpha(int c);
 int	    ft_isalnum(int c);
 int     ft_toupper(int c);
 int     ft_tolower(int c);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
