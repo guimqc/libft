f4ee34a239c82801920c29badb0817386ad97547
vial <guillaumevial15@gmail.com>
2022-02-23 16:15:50 -0500

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -8,5 +8,5 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
     while (++i < n)
         if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
             return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-    return (0);
+    return (0); 
 }
