457a0a6a7fece1a9ba638870c307064fbf7da385
vial <guillaumevial15@gmail.com>
2022-02-23 16:13:11 -0500

    memchr_optimized

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,12 +3,10 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t i;
-	unsigned char *str;
 
-	str = (unsigned char *)s;
 	i = -1;
 	while (++i < n)
-		if (str[i] == (unsigned char)c)
-			return (&str[i]);
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return (&((void *)s)[i]);
 	return (NULL);
 }
