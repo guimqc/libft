e13bfb3303ee6248e412acfb26645ee3dd203295
vial <guillaumevial15@gmail.com>
2022-02-24 14:34:58 -0500

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,24 +1,28 @@
 #include "libft.h"
-#include <string.h>
-#include <stdio.h>
 
-char    *strnstr(const char *haystack, const char *needle, size_t len)
+static int is_valid(char *haystack, char *needle, size_t i)
 {
-    size_t i;
+	int ii;
 
-    if (ft_strlen((char *)needle) > ft_strlen((char *)haystack))
-        return (NULL);
-    if ((char *)needle == 0)
-        return ((char *)haystack);
-    i = -1;
-    while (++i < len)
-        //
-    return (NULL);
+	ii = 0;
+	while (haystack[i] && needle[ii])
+		if (haystack[i++] != needle[ii++])
+			return (0);
+	return (1);
 }
 
-int main()
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    const char haystack[] = "hello world";
-    const char needle[] = "hello world1";
-    printf("%s", strnstr(haystack, needle, 11)); // = NULL
+	size_t i;
+
+	if (ft_strlen((char *)needle) > ft_strlen((char *)haystack))
+		return (NULL);
+	if (ft_strlen((char *)needle) == 0)
+		return ((char *)haystack);
+	i = -1;
+	while (++i < len && ((char *)haystack)[i])
+		if (((char *)haystack)[i] == ((char *)needle)[0])
+			if (is_valid((char *)haystack, (char *)needle, i) == 1)
+				return (&((char *)haystack)[i]);
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -5,6 +5,7 @@
 
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 size_t  ft_strlen(char *str);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
