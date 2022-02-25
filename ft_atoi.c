e20df13b58585cc87899348198f15b635d716df3
vial <guillaumevial15@gmail.com>
2022-02-25 18:44:09 -0500

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -45,9 +45,6 @@ int	ft_atoi(const char *str)
 	}
 	result = 0;
 	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + str[i] - 48;
-		i++;
-	}
+		result = result * 10 + str[i++] - 48;
 	return (sign * result);
 }
