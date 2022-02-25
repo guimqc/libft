5f952bc62e7600b17a643f2ddb76ad50db761f3c
vial <guillaumevial15@gmail.com>
2022-02-25 15:50:40 -0500

    bzero_optimized

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,10 +3,8 @@
 void	ft_bzero(void *s, size_t n)
 {
 	size_t  i;
-	char *converted_str;
 
-	converted_str = (char *)s;
 	i = -1;
 	while (++i < n)
-		converted_str[i] = 0;
+		((char *)s)[i] = 0;
 }
