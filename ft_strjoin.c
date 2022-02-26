0c1257e3b120f7503c66e7ba8b90004c42dd607e
vial <guillaumevial15@gmail.com>
2022-02-26 13:48:50 -0500

    strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+	int strsize;
+	size_t i;
+
+	strsize = ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1;
+	str = malloc(sizeof(char) * strsize);
+	i = -1;
+	while (++i < ft_strlen((char *)s1))
+		str[i] = s1[i];
+	i = -1;
+	while (++i < ft_strlen((char *)s2))
+		str[i + ft_strlen((char *)s1)] = s2[i];
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -3,9 +3,11 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+
 char    *ft_strdup(const char *s1);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
+char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 size_t  ft_strlen(char *str);
