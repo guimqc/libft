e34d92dc10e50dad7eeeeb349f9010b5c6fb8fb9
vial <guillaumevial15@gmail.com>
2022-02-26 11:55:39 -0500

    substr_fix

--- ft_substr.c
+++ ft_substr.c
@@ -1,13 +1,17 @@
 #include "libft.h"
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *str;
 	size_t i;
-
-	str = malloc(sizeof(char) * len);
+	
+	if (len > ft_strlen((char *)s))
+		len = ft_strlen((char *)s);
+	str = malloc(sizeof(char) * len + 1);
+	if (start >= ft_strlen((char *)s))
+		return (str);
 	i = -1;
-	while (++i < len)
+	while (++i < len && s[i])
 		str[i] = s[start + i];
 	return (str);
 }
