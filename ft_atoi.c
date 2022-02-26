f7d022299a48c8b7f44687fb8c5bdd5fbf72c8a9
vial <guillaumevial15@gmail.com>
2022-02-25 20:22:28 -0500

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,8 +3,7 @@ static int check_space(char *str)
 	int i;
 
 	i = 0;
-	while ((str[i] == '\t') || (str[i] == '\n') || (str[i] == '\v') || (str[i] == '\f')
-				|| (str[i] == 'r') || (str[i] == ' '))
+	while ((str[i] == ' ') || (str[i] >= 9 && str[i] <= 13))
 			i++;
 	return (i);
 }
@@ -13,19 +12,14 @@ static int check_sign(char *str)
 {
 	int i;
 	int sign;
-	int sign_met;
-	
+
 	sign = 1;
-	sign_met = 0;
 	i = check_space(str);
-	while(str[i] == '+' || str[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 	{
-		if (sign_met == 1)
-			break ;
 		if (str[i] == '-')
- 			sign *= -1;
-		sign_met = 1;
- 		i++;
+			sign *= -1;
+		i++;
 	}
 	return (sign * i);
 }
