c821d10ccb53df4a8f136c619dc4e342e2c27006
vial <guillaumevial15@gmail.com>
2022-02-26 23:02:18 -0500

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,4 +1,5 @@
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -7,16 +8,26 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	i = 0;
 	while (s1[i] == ((char *)set)[0])
+	{
 		i++;
+	}
 	j = ft_strlen((char *)s1);
-	while (s1[i] == ((char *)set )[0])
+	while (s1[j] == ((char *)set )[0])
+	{
+		printf("testj\n");
 		j--;
+	}
+	printf("i = %d\n", i);
+	printf("j = %lu\n", ft_strlen((char *)s1) - j - i - 1);
 	return (ft_substr(s1, i, ft_strlen((char *)s1) - j));
 }
 
 int main()
 {
-	
+	char *str = "oooell";
+	char *set = "o";
+
+	printf("%s\n", ft_strtrim(str, set));
 }
 
 // i; would be the start of the substr and j; the len (ft_strlen(s1) - j);
