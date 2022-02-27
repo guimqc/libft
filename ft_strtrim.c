8f8803c1dde7fc3817fda5a3b750a8f69d858dfb
vial <guillaumevial15@gmail.com>
2022-02-27 10:52:30 -0500

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,27 +5,24 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int i;
 	int j;
+	int minus;
+	int s1_len;
 
+	s1_len = ft_strlen((char *)s1);
 	i = 0;
-	while (s1[i] == ((char *)set)[0])
-	{
+	while (s1[i] == set[0])
 		i++;
-	}
-	j = ft_strlen((char *)s1) - 1;
-	while (s1[j] == ((char *)set)[0])
+	j = s1_len - 1;
+	while (s1[j] == set[0])
 		j--;
-	printf("%d\n", j);
-	return (ft_substr(s1, i, j - i + 1));
+	minus = s1_len - j - 1;
+	return (ft_substr(s1, i, s1_len - minus - i));
 }
 
 int main()
 {
-	char *str = "lololl";
+	char *str = "llsooollll";
 	char *set = "l";
 
 	printf("%s\n", ft_strtrim(str, set));
 }
-
-// i; would be the start of the substr and j; the len (ft_strlen(s1) - j);
-// i++; --j; substr(s1, i, ft_strlen(s1) - j);
-// set could be multiple chars tho, so idk about that yet.
