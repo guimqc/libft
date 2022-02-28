b1c24a0246c0c0030ecb3a78c67cbca618544b20
vial <guillaumevial15@gmail.com>
2022-02-28 11:30:54 -0500

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,5 +1,4 @@
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -10,19 +9,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	s1_len = ft_strlen((char *)s1);
 	i = 0;
-	while (s1[i] == set[0])
+	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
 	j = s1_len - 1;
-	while (s1[j] == set[0])
+	while (s1[j] && ft_strchr(set, s1[j]))
 		j--;
 	minus = s1_len - j - 1;
 	return (ft_substr(s1, i, s1_len - minus - i));
-}
-
-int main()
-{
-	char *str = "llsooollll";
-	char *set = "l";
-
-	printf("%s\n", ft_strtrim(str, set));
 }
