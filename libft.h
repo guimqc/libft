bf6ceb79dcd9d6f402fc1829a33e8b1c5e8992ee
vial <guillaumevial15@gmail.com>
2022-03-02 17:45:24 -0500

    split

--- libft.h
+++ libft.h
@@ -4,10 +4,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
 char    *ft_strdup(const char *s1);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
+char    **ft_split(const char *s, char c);
 char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
