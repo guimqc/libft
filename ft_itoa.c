67f96ac13941271d829ff51444f8c7f91512b1d5
vial <guillaumevial15@gmail.com>
2022-03-05 19:08:36 -0500

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -25,6 +25,7 @@ char    *ft_itoa(int n)
 		n *= -1;
 	s = ft_calloc (find_len(n) + 1, sizeof(char));
 	i = find_len(n);
+	s[i] = '\0';
 	while (--i >= 0)
 	{
 		s[i] = n % 10 + 48;
@@ -35,7 +36,7 @@ char    *ft_itoa(int n)
 
 int main()
 {
-	int x = +234;
-	printf("%i\n", find_len(x));
+	int x = 234;
+	printf("alloc %i char\n", find_len(x));
 	printf("%s\n", ft_itoa(x));
 }
