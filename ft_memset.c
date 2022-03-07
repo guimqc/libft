24177ae6e6dd30d75c02e519e8fdb1bf5306d8bb
vial <guillaumevial15@gmail.com>
2022-03-07 11:03:38 -0500

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -2,10 +2,7 @@
 
 void	*ft_memset(void *str, int c, size_t len)
 {
-	size_t i;
-
-	i = -1;
-	while (++i < len)
-		((unsigned char *)str)[i] = (unsigned char)c;
+	while (len-- > 0)
+		((unsigned char *)str)[len] = (unsigned char)c;
 	return	(str);
 }
