592c5d23a69d114f245704f193b05e453ffcacbc
vial <guillaumevial15@gmail.com>
2022-03-07 10:46:51 -0500

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,10 +2,7 @@
 
 void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
-
-	i = -1;
-	while (++i < n)
-		((char *)dst)[i] = ((char *)src)[i];
+	while (n-- > 0)
+		((char *)dst)[n] = ((char *)src)[n];
 	return (dst);
 }
