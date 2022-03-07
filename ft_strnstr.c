3a60b14ff70b100092e69b1c9a20e636d48f5111
vial <guillaumevial15@gmail.com>
2022-03-07 15:44:45 -0500

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,12 +1,11 @@
 #include "libft.h"
 
-static int is_valid(char *haystack, char *needle, size_t i)
+static int is_valid(char *haystack, char *needle, size_t i, size_t len)
 {
 	int ii;
-
 	ii = 0;
 	while (haystack[i] && needle[ii])
-		if (haystack[i++] != needle[ii++])
+		if (haystack[i++] != needle[ii++] || i > len)
 			return (0);
 	return (1);
 }
@@ -20,7 +19,7 @@ char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 	i = -1;
 	while (++i < len && ((char *)haystack)[i])
 		if (((char *)haystack)[i] == ((char *)needle)[0])
-			if (is_valid((char *)haystack, (char *)needle, i) == 1)
+			if (is_valid((char *)haystack, (char *)needle, i, len) == 1)
 				return (&((char *)haystack)[i]);
 	return (NULL);
 }
