0edcec4144b9bf76f7084aeb581053e816e7351c
vial <guillaumevial15@gmail.com>
2022-03-12 18:00:54 -0500

    lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -4,8 +4,9 @@ void ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list *last;
 
-	if (!lst)
-		*lst = new;
 	last = ft_lstlast(*lst);
-	last->next = new;
+	if (*lst)
+		last->next = new;
+	else
+		*lst = new;
 }
