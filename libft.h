3fb5e048dd298615b3eaf6b6f21c2ebb16ec7925
vial <guillaumevial15@gmail.com>
2022-03-11 21:36:56 -0500

    striteri

--- libft.h
+++ libft.h
@@ -25,6 +25,7 @@ void	*ft_memset(void *str, int c, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void  	*ft_memcpy(void *dst, const void *src, size_t n);
 void  	*ft_memmove(void *dst, const void *src, size_t len);
+void    ft_striteri(char *s, void (*f)(unsigned int, char*));
 int     ft_isdigit(int c);
 int     ft_isalpha(int c);
 int	    ft_isalnum(int c);
