56a6365cb295611aa3b90f96e050a8c5ddd3a572
vial <guillaumevial15@gmail.com>
2022-03-11 21:20:09 -0500

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,16 +1,13 @@
+#include "libft.h"
+
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char *str;
 	size_t i;
 
 	str = ft_calloc(ft_strlen((char *)s), sizeof(char));
-	i = 0;
-	while (i < ft_strlen((char *)s))
-		str[i] = // apply func on s[i]
+	i = -1;
+	while (++i < ft_strlen((char *)s))
+		str[i] = f(i, str[i]);
 	return (str);
-}
-
-int main()
-{
-	char *s = "bcde";
 }
