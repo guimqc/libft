763c8b7bb1792a3718fd391326b654a8d4bffb19
vial <guillaumevial15@gmail.com>
2022-03-11 21:42:55 -0500

    putchar_fd

--- libft.h
+++ libft.h
@@ -20,6 +20,7 @@ size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void    ft_putchar(char c);
 void    ft_putstr(char *str);
 void    ft_bzero(void *s, size_t n);
+void    ft_putchar_fd(char c, int fd);
 void    *ft_calloc(size_t count, size_t size);
 void	*ft_memset(void *str, int c, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
