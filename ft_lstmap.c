5956bf220162971af0a7bc1b4768fc0246b94a7c
vial <guillaumevial15@gmail.com>
2022-03-16 21:15:10 -0400

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,15 +3,11 @@
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
     t_list *arr;
-    int i;
 
-    arr = ft_calloc(ft_lstsize(lst), sizeof(t_list));
-    i = 0;
+    arr = ft_lstnew(f(lst->content));
     while (lst)
     {
-        arr[i++] = *ft_lstnew(f(lst->content));
-        ft_lstdelone(lst, del);
-        lst = lst->next;
+        
     }
     return (arr);
 }
