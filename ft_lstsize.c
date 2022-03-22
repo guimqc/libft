233cad76dc096178a46341b29092a7877656a894
vial <guillaumevial15@gmail.com>
2022-03-22 14:51:23 -0400

    lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,10 @@ int ft_lstsize(t_list *lst)
     if (lst == NULL)
         return (0);
     i = 0;
-    while (lst[i].next != NULL)
+    while (lst->next != NULL)
+    {
+        lst = lst->next;
         i++;
+    }
     return (i + 1);
 }
