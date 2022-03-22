cfce0a25b5cab4ffb67c75ff9ef53c8150d86dc0
vial <guillaumevial15@gmail.com>
2022-03-22 14:51:54 -0400

    lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -2,14 +2,14 @@
 
 int ft_lstsize(t_list *lst)
 {
-    int i;
-    if (lst == NULL)
-        return (0);
-    i = 0;
-    while (lst->next != NULL)
-    {
-        lst = lst->next;
-        i++;
-    }
-    return (i + 1);
+	int i;
+	if (lst == NULL)
+		return (0);
+	i = 0;
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i + 1);
 }
