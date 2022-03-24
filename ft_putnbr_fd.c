b0497723ba7c306768c1a3ac3b573ba1832ea91f
vial <guillaumevial15@gmail.com>
2022-03-24 17:39:23 -0400

    putbnr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,10 +1,25 @@
 #include "libft.h"
+#include <stdio.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char *s;
+	char num;
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		num = (n % 10) + 48;
+		write(fd, &num, 1);
+		n /= 10;
+	}
+}
 
-	s = ft_itoa(n);
-	write(fd, s, ft_strlen(s));
-	free(s);
+int main()
+{
+	int n = 1234;
+
+	ft_putnbr_fd(n, 1);
 }
