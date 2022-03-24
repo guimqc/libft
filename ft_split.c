296c846bd2a37edc03d8705bc06c3f44457fda33
vial <guillaumevial15@gmail.com>
2022-03-24 03:09:18 -0400

    split

--- ft_split.c
+++ ft_split.c
@@ -20,45 +20,48 @@ static size_t split_count(char *s, char c)
 	return (split_count);
 }
 
-char **ft_split(const char *s, char c)
+char **alloc(char *s, char c)
 {
-	char	*ptr;
-	char	**arr;
-	size_t	i;
-	size_t	ii;
-	size_t	count;
+	char **arr;
+	size_t i;
+	size_t ii;
+	size_t count;
 
-	arr = malloc(sizeof(char *) * split_count((char *)s, c));
+	arr = ft_calloc(split_count(s, c), sizeof(char *));
 	i = -1;
 	ii = 0;
 	count = 0;
-	while ((++i < ft_strlen((char *)s)) && (ii < split_count((char *)s, c)))
+	while (++i < ft_strlen(s))
 	{
-		if (((char *)s)[i] != c)
+		if (s[i] != c)
 			count++;
-		if (((char *)s)[i] != c && (((char *)s)[i + 1] == c || i == ft_strlen((char *)s) - 1))
+		if ((s[i] == c && s[i - 1] != c && i != 0) || (i == ft_strlen(s) - 1 && s[i] != c))
 		{
-			ptr = malloc(sizeof(char) * (count + 1));
-			ptr = ft_substr(s, i - count + 1, count);
-			arr[ii] = malloc(sizeof(char) * (count + 1));
-			ft_strlcpy(arr[ii++], ptr, count + 1);
+			arr[ii++] = ft_calloc(count, sizeof(char));
 			count = 0;
-			free(ptr);
 		}
 	}
-	arr[ii] = 0;
 	return (arr);
 }
 
-// int main()
-// {
-// 	const char *s = ",,hello,,,bonjour,,,hola,,,,,salve,,";
-// 	char c = ',';
-// 	char **arr = ft_split(s, c);
+char **ft_split(const char *s, char c)
+{
+	char	**arr;
 
-// 	size_t i;
-// 	i = -1;
-// 	while (++i < split_count((char *)s, c))
-// 		printf("%s\n", arr[i]);
-// }
+	arr = alloc((char *)s, c);
+
+	return (arr);
+}
+
+int main()
+{
+	const char *s = "hello,,,bonjour,,,hola,,,,,salve";
+	char c = ',';
+	char **arr = ft_split(s, c);
+
+	size_t i;
+	i = -1;
+	while (++i < split_count((char *)s, c))
+		printf("%s\n", arr[i]);
+}
 // make split to test
