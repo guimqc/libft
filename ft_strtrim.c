93b9581798a6f5e53b9fe7ba703650ca17c5b6d5
vial <guillaumevial15@gmail.com>
2022-03-28 20:13:44 -0400

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -16,7 +16,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int	i;
 	int	j;
-	int	minus;
 	int	s1_len;
 
 	s1_len = ft_strlen((char *)s1);
@@ -24,15 +23,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
 	j = s1_len - 1;
-	while (s1[j] && ft_strchr(set, s1[j]))
+	while (j && ft_strchr(set, s1[j]))
 		j--;
-	minus = s1_len - j - 1;
-	return (ft_substr(s1, i, s1_len - minus - i));
-}
-
-int main()
-{
-	char *str = ",,,,,,,,,";
-
-	printf("%s\n ", ft_strtrim(str, ",")); // should return NULL
+	return (ft_substr(s1, i, s1_len - (s1_len - j - 1) - i));
 }
