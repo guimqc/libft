0202b528cd657a1d31d7b34b19a5691b13bf70e0
vial <guillaumevial15@gmail.com>
2022-03-28 20:14:50 -0400

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
