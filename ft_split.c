9eada663986a04ce3d40b3d09c7c47f8ac338e71
vial <guillaumevial15@gmail.com>
2022-03-30 16:54:20 -0400

    split

--- ft_split.c
+++ ft_split.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 static size_t	split_count(char *s, char c)
 {
@@ -27,6 +28,7 @@ static size_t	split_count(char *s, char c)
 			split_count++;
 		i++;
 	}
+	//printf("split count : %zu\n", split_count);
 	return (split_count);
 }
 
@@ -78,5 +80,17 @@ char	**ft_split(const char *s, char c)
 		}
 	}
 	arr[++ii] = 0;
+	//printf("last tab : %zu\n", ii);
 	return (arr);
 }
+
+// int main()
+// {
+// 	const char *s = "     ";
+// 	char **arr = ft_split(s, ' ');
+// 	int i = -1;
+// 	while (arr[++i])
+// 	{
+// 		printf("%s\n", arr[i]);
+// 	}
+// }
