d7b6f864fdf213523677626534f7ee9cee6edc6a
vial <guillaumevial15@gmail.com>
2022-03-31 15:32:06 -0400

    lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -15,7 +15,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*node;
 
-	node = ft_calloc(1, sizeof(node));
+	node = ft_calloc(1, sizeof(t_list));
 	node->content = content;
 	node->next = NULL;
 	return (node);
