266156561c530ff30d5cb9d908cd404f6b928ee4
vial <guillaumevial15@gmail.com>
2022-04-02 11:16:53 -0400

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,8 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*p;
 
 	p = malloc(size * count);
-	if (!p)
-		return (NULL);
-	ft_bzero(p, (count * size));
+	if (p)
+		ft_bzero(p, (count * size));
 	return (p);
 }
