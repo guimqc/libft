ff223e32ed1fae7ba3aef0b7b9aac7d3eb73fe91
vial <guillaumevial15@gmail.com>
2022-04-02 13:54:35 -0400

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,30 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
-static int	is_valid(char *haystack, char *needle, size_t i, size_t len)
-{
-	int	ii;
-
-	ii = 0;
-	while (haystack[i] && needle[ii])
-		if (haystack[i++] != needle[ii++] || i > len)
-			return (0);
-	return (1);
-}
+#include <stdio.h>
+#include <string.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-
-	if (ft_strlen((char *)needle) == 0)
+	if (!(*needle))
 		return ((char *)haystack);
-	i = -1;
-	while (++i < len && ((char *)haystack)[i])
-		if (((char *)haystack)[i] == ((char *)needle)[0])
-			if (is_valid((char *)haystack, (char *)needle, i, len) == 1)
-				return (&((char *)haystack)[i]);
+	while (len > 0)
+	{
+		if (haystack[len - 1] == needle[ft_strlen((char *)needle - 1)])
+			printf("%c\n", haystack[len - 1]);
+		len--;
+	}
 	return (NULL);
 }
 
-// correct this and libft is moulinette approuved
+int main()
+{
+	const char *haystack = "this is a test";
+	const char *needle = "test";
+
+	printf("%s\n", ft_strnstr(haystack, needle, 15));
+}
