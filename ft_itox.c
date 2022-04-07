7486a05cc1f30d7b6bae0989ae6f7da9899cff72
Guillaume Vial <gvial@c1r5p7.42quebec.com>
2022-04-07 16:29:36 -0400

    itox

--- ft_itox.c
+++ ft_itox.c
@@ -33,8 +33,6 @@ char	*ft_itox(unsigned int n, int caps)
 	char	*hex;
 	char	*result;
 
-	if (n == -2147483648)
-		write(1, "80000000", 8);
 	len = find_len(n);
 	hex = "0123456789abcdef";
 	result = ft_calloc(len + 1, sizeof(char));
