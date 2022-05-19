155e5e57327a08039830abbad837d7a874700060
Guillaume Vial <gvial@c3r5p3.42quebec.com>
2022-05-19 12:29:50 -0400

    norm .h

--- libft.h
+++ libft.h
@@ -22,7 +22,6 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
